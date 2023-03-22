a94dcee3ca02dad554c09fa574f9c5bf59428d02
opilane <opilane@HP-CZC0359NN5>
2023-03-22 09:50:36 +0200

    Add project files.

---
+++ ProductList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LINQi_töö
+{
+    public class ProductList
+    {
+        public static readonly List<Product> products = new List<Product>
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Juice",
+            },
+            new Product()
+            {
+                Id = 2,
+                Name = "Cheese",
+            },
+            new Product()
+            {
+                Id = 3,
+                Name = "Apple",
+            },
+            new Product()
+            {
+                Id = 4,
+                Name = "Banana",
+            },
+            new Product()
+            {
+                Id = 5,
+                Name = "Jar jam",
+            },
+            new Product()
+            {
+                Id = 6,
+                Name = "Chips",
+            },
+            new Product()
+            {
+                Id = 7,
+                Name = "Milk",
+            }
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LINQi_töö
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThenBy();
+            Console.WriteLine("---------------------");
+            LastOrDefault();
+            Console.WriteLine("---------------------");
+        }
+        //ThenBy sorteerib muutujaid pärast OrderBy-t.
+        //ThenBy sorteerib üldiselt ja OrderBy sorteerib spetsiifilises järjekorras.
+        public static void ThenBy()
+        {
+            Console.WriteLine("Then By");
+            Console.WriteLine("Product list");
+            
+            var thenByResult = ProductList.products
+                .OrderBy(x => x.Id)
+                .ThenBy(x => x.Name); //Siin on produktid üldises järjekordades pandud
+
+            foreach (var product in thenByResult)
+            {
+                Console.WriteLine("Product name: " + product.Name + " | " + "Product Id: " + product.Id);
+            }
+
+        }
+        //LastOrDefault kasutatakse selleks, et näidata või kustutada viimast muutujat ning jätab teised muutujad välja.
+        //Kui on puudujääk e. default ehk mingi muutuja ei eksisteeri koodis, siis näitab selle asemel nulli.
+        public static void LastOrDefault()
+        {
+            Console.WriteLine("Last Or Default");
+            
+            var lastOrDefault = ProductList.products.LastOrDefault();
+            var lastOrDefault2 = ProductList.products.LastOrDefault();
+
+            Console.WriteLine("Product: " + lastOrDefault.Name + " | " + "Id: " + lastOrDefault2.Id);
+
+        }
+      
+
+
+
+    }
+}
